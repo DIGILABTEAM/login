3b08654480b0e3fd0eb8eca3ff5ac1e3b2c9071c
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-10-20 18:31:29 +0530

    deployment
    Project testing with html, js and css

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/* ======================================================================
+  Author Custom JavaScript
+====================================================================== */
+// Loop through Array of Objects
+var objPeople = [
+	{ // Object @ 0 index
+		username: "digilab",
+		password: "digi123"
+	},
+	{ // Object @ 1 index
+		username: "mohanish",
+		password: "12345"
+	},
+	{ // Object @ 2 index
+		username: "Engelsir",
+		password: "54321"
+	}
+
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			console.log(username + " is logged in!!!")
+			// stop the function if this is found to be true
+			return
+		}
+	}
+	alert("incorrect username or password");
+}
